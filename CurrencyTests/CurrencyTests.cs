f08bf30b144a6c746f34f7c6cf05ec0a227f161a
Sreedevi Cherukuri <sreedevi.cherukuri@outlook.com>
2024-06-17 17:46:57 +0530

    Added new unit tests

--- CurrencyTests/CurrencyTests.cs
+++ CurrencyTests/CurrencyTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Currency;
+using System;
+using System.Linq;
 
 namespace TestProject1
 {
@@ -7,7 +9,7 @@ namespace TestProject1
     public class CurrencyTests
     {
         [TestMethod]
-        public void ValidateDenominations()
+        public void ValidateNoteAndCoinDenominations()
         {
             CurrencyDenominations denominations = new CurrencyDenominations(14.5);
             int[] noteDenominations = denominations.noteDenominations;
@@ -15,6 +17,33 @@ namespace TestProject1
             Assert.AreEqual(1, noteDenominations[3]);
             int[] coinDenominations = denominations.coinsDenominations;
             Assert.AreEqual(1, coinDenominations[0]);
+        }
+
+        [TestMethod]
+        public void ValidateOnlyNoteDenominations()
+        {
+            CurrencyDenominations denominations = new CurrencyDenominations(25);
+            int[] noteDenominations = denominations.noteDenominations;
+            Assert.AreEqual(1, noteDenominations[2]);
+            Assert.AreEqual(1, noteDenominations[4]);
+        }
+
+        [TestMethod]
+        public void ValidateOnlyCoinDenominations()
+        {
+            CurrencyDenominations denominations = new CurrencyDenominations(0.5);
+            int[] coinDenominations = denominations.coinsDenominations;
+            Assert.AreEqual(1, coinDenominations[0]);
+        }
+
+        [TestMethod]
+        public void ValidateZeroAmount()
+        {
+            CurrencyDenominations denominations = new CurrencyDenominations(0);
+            int[] noteDenominations = denominations.noteDenominations;
+            int[] coinDenominations = denominations.coinsDenominations;
+            Assert.AreEqual(noteDenominations.All(e => e == 0), true);
+            Assert.AreEqual(coinDenominations.All(e => e == 0), true);
         }
     }
 }
